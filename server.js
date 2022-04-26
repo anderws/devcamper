5ef7afb704a73d70997d308557e9da8bdd8ff646
anderws <anderws10@gmail.com>
2022-04-26 18:02:26 -0300

    add bootcamp route

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config/config.env' });
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
